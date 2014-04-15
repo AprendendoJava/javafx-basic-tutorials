6aede9ef6f3ef95210ec790e5b8cfbc58098b034
jesuino <william.a.siqueira@gmail.com>
2014-04-15 13:00:41 -0300

    Adicionando JS da postagem 12

---
+++ blog-javafx/post-12-javascript/README.md
@@ -0,0 +1,7 @@
+Use o seguinte comando para rodar esse arquivo:
+
+$ jjs -fx helloJavaFX.js
+
+Mais informações em: 
+
+http://aprendendo-javafx.blogspot.com.br/
--- blog-javafx/post-12-javascript/helloJavaFX.js
+++ blog-javafx/post-12-javascript/helloJavaFX.js
@@ -1,1 +1,6 @@
-//Stuff
+var txtOla = new javafx.scene.control.Label(">> Olá Mundo! <<")
+txtOla.font = new javafx.scene.text.Font(50)
+$STAGE.scene = new javafx.scene.Scene(txtOla)
+$STAGE.width = 500
+$STAGE.title = "Olá JavaFX + Javascript"
+$STAGE.show()
